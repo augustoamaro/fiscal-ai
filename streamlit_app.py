f2d4092ea4e2e4c66fb288ab53d5de8bdb6bd98d
Leônidas Augusto Amaro <augustoamaro@proton.me>
2024-07-13 18:23:18 -0300

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,13 @@
 import streamlit as st
 
+upload_and_analyze_page = st.Page(
+    "upload_and_analyze.py", title="Upload e Análise", icon=":material/upload:")
+individual_page = st.Page("individual_analysis.py",
+                          title="Análise Individual", icon=":material/pageview:")
+reports_page = st.Page("reports.py", title="Relatórios",
+                       icon=":material/assessment:")
 
-def main():
-
-    upload_and_analyze_page = st.Page(
-        "upload_and_analyze.py", title="Upload e Análise", icon=":material/upload:")
-    individual_page = st.Page("individual_analysis.py",
-                              title="Análise Individual", icon=":material/pageview:")
-    reports_page = st.Page("reports.py", title="Relatórios",
-                           icon=":material/assessment:")
-
-    pg = st.navigation(
-        [upload_and_analyze_page, individual_page, reports_page])
-    st.set_page_config(page_title="FiscAI", page_icon=":material/edit:")
-    pg.run()
-
-
-if __name__ == "__main__":
-    main()
+pg = st.navigation(
+    [upload_and_analyze_page, individual_page, reports_page])
+st.set_page_config(page_title="FiscAI", page_icon=":material/edit:")
+pg.run()
